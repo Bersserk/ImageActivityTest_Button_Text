a57b99bd46cb20aff34c73ea62926b9db43f50a2
Bersserk <olegbers@gmail.com>
2015-05-08 23:32:07 +0300

    добавлена кнопка. настроена работа двух кнопок через один обработчик с  разными результатами

--- app/src/main/java/ua/kh/bersserk/imageactivitytest/MainActivity.java
+++ app/src/main/java/ua/kh/bersserk/imageactivitytest/MainActivity.java
@@ -2,21 +2,23 @@ package ua.kh.bersserk.imageactivitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
+//import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+//import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.Toast;
+
 
 
 public class MainActivity extends Activity {
 
     // объявим наши объекты.
 
-    private boolean tp;
-    private TextView myTextView;
-    private Button myButton;
+  //  public boolean tp;
+    TextView myTextView;
+
+
 
 
     @Override
@@ -24,7 +26,32 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // найдем наше изображение
+        // находим наши кнопки
+        Button myButton1 = (Button) findViewById(R.id.myButton1);
+        Button myButton2 = (Button) findViewById(R.id.myButton2);
+
+        //устанавливаем общий обработчик
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.myButton1:
+                        myTextView.setText(R.string.textBtn1);
+                        break;
+                    case R.id.myButton2:
+                        myTextView.setText("");
+                        break;
+
+                }
+
+            }
+        };
+
+        myButton1.setOnClickListener(onClickListener);
+        myButton2.setOnClickListener(onClickListener);
+
+        /* найдем наше изображение
         final ImageView sniperImg = (ImageView) findViewById(R.id.imageView);
 
         //зададим метод OnClick для картинки;
@@ -35,44 +62,19 @@ public class MainActivity extends Activity {
                 sniperImg.setImageResource(tp ? R.drawable.sniper : R.drawable.oficer);
                 return false;
             }
-        });
 
 
-
-
-        //найдем текст и кнопку
-        myTextView = (TextView) findViewById(R.id.myTextView);
-        myButton = (Button) findViewById(R.id.myButton);
-
-        //зададим обработчик событий и метод OnClick для кнопки
-        myButton.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                myTextView.setText("Ку-ку ёпта!");
-
+            public void showToast(View view) {
+                //создаем и отображаем текстовое уведомление
+                int duration = Toast.LENGTH_LONG;
+                Toast toast2 = Toast.makeText(getApplicationContext(),
+                        "Это картинка!",
+                        duration);
+                toast2.setGravity(Gravity.TOP, 0, 0);
+                toast2.show();
             }
-        });
-
-        //зададим обработчик событий и метод OnClick для текста
-        myTextView.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                myTextView.setText("TEXT");
-            }
-
-
-
-
-        });
-    }
-
-    public void showToast(View view) {
-        //создаем и отображаем текстовое уведомление
-        int duration = Toast.LENGTH_LONG;
-        Toast toast2 = Toast.makeText(getApplicationContext(),
-                "Это картинка!",
-                duration);
-        toast2.setGravity(Gravity.TOP, 0, 0);
-        toast2.show();
+            });
+        } */
     }
 }
+
